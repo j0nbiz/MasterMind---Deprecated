876a0479f2c313cad82188cdd75d963fb4f42573
Jonathan Bizier <j0nbiz@outlook.com>
2016-09-28 10:54:34 -0400

    Initial commit Created class skeletons

---
+++ src/main/java/team/six/mastermind/server/MMServerSession.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.six.mastermind.server;
+
+/**
+ *
+ * @author 1437203
+ */
+public class MMServerSession {
+    
+}
