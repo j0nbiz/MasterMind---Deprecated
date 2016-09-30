834d749803514b2c4961e7a32d00c345139d71d0
Jonathan Bizier <j0nbiz@outlook.com>
2016-09-30 15:40:17 -0400

    started working on the skeleton of the mastermind server class

--- src/main/java/team/six/mastermind/common/MMPacket.java
+++ src/main/java/team/six/mastermind/common/MMPacket.java
@@ -5,10 +5,26 @@
  */
 package team.six.mastermind.common;
 
+import java.net.*;
+import java.io.*; 
+
 /**
  *
  * @author 1437203
  */
 public class MMPacket {
+    // Bytes to be sent
+    private byte mmBytes[] = new byte[4];
     
+    public MMPacket(byte comp1, byte comp2, byte comp3, byte comp4) throws IOException{
+        // Putting components into byte array
+        mmBytes[0] = comp1;
+        mmBytes[1] = comp2;
+        mmBytes[2] = comp3;
+        mmBytes[3] = comp4;
+    }
+
+    public byte[] getBytes(){
+        return mmBytes;
+    }
 }
--- src/main/java/team/six/mastermind/server/MMServer.java
+++ src/main/java/team/six/mastermind/server/MMServer.java
@@ -5,10 +5,61 @@
  */
 package team.six.mastermind.server;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import team.six.mastermind.common.MMPacket;
+
 /**
  *
  * @author 1437203
  */
 public class MMServer {
+
+    private ServerSocket serverSocket;
+    private List<Socket> clientSockets;
     
+    private static final int BUFFSIZE = 32;
+    private byte[] incPackets = new byte[BUFFSIZE];
+    private int incPacketsSize;
+
+    public MMServer(ServerSocket serversock) {
+        this.serverSocket = serversock;
+        
+        // Automaticaly start the server
+    }
+
+    public void start() throws IOException {
+        for (;;) {
+            // Check for potential new connections
+            Socket client = serverSocket.accept();
+            
+            // Check for new game request
+            while ((incPacketsSize = client.getInputStream().read(incPackets)) != -1) {
+                //Add new connection to list if requested
+                if(client.getInputStream().read() == 0x00000000){
+                    clientSockets.add(client);
+                    // TODO: Send a response
+                }
+            }
+            
+            // Act upon all current connections
+            for(Socket conn: clientSockets){
+                
+                // Listen
+                while ((incPacketsSize = conn.getInputStream().read(incPackets)) != -1) {
+                    // Fill buffer
+                }
+                
+                // Interpret message
+                
+                // Reply message
+            }
+            
+            // Check for disconnections
+            
+            
+            // clntSock.close();
+        }
+    }
 }
